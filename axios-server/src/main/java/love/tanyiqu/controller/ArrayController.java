93e096655ae2525070658bfb8e2d44ea0561d8bc
Tanyiqu <1953649096@qq.com>
2021-04-04 18:13:55 +0800

    传入数组 post

--- axios-server/src/main/java/love/tanyiqu/controller/ArrayController.java
+++ axios-server/src/main/java/love/tanyiqu/controller/ArrayController.java
@@ -1,4 +1,23 @@
 package love.tanyiqu.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/array")
 public class ArrayController {
+
+
+    @PostMapping("/post")
+    public String post(Integer[] ids) {
+        System.out.println("params: " + Arrays.toString(ids));
+        return "array post success";
+    }
+
 }
---
+++ axios-study/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <button @click="arrayPost">array post</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+  name: 'App',
+  components: {
+  },
+
+
+  methods: {
+    arrayPost() {
+      console.log('send array post');
+      axios.post('http://127.0.0.1:8866/array/post', qs.stringify(
+        {
+          ids: [1, 2, 3]
+        },
+        {
+          indices: false
+        }
+      )).then(data => {
+        console.log(data.data);
+      });
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
